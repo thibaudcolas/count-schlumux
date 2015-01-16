150303a332b3a263f2e81a199d2f3a54648c166d
Thibaud Colas <thibaudcolas@gmail.com>
2015-01-16 19:13:54 +1300

    Add missing semicolon

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 // app.param('counter', function(req, res, next, counter){
 //   req.counter =
 //   return next()
-// })
+// });
 
 app.put('/count/:counter', function(req, res) {
   res.send(req.params.counter);
